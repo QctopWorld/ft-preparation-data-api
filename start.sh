e6625f85a3b421ab69d78446a5b2c31331645289
Mickael <r.jaonamickael@gmail.com>
2025-06-09 12:10:12 -0400

    vers wordpress

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-uvicorn app:app --host 0.0.0.0 --port 10000
+# active le venv situé à la racine du projet
+source "$(dirname "$0")/venv/bin/activate"  
+# puis lance Uvicorn
+exec uvicorn app:app \
+     --host 127.0.0.1 \
+     --port 9000 \
+     --reload
