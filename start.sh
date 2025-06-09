7a78f8abbd8533426c477663b7bf05312e431a7b
Mickael <r.jaonamickael@gmail.com>
2025-06-09 12:11:06 -0400

    re

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # active le venv situé à la racine du projet
-source "$(dirname "$0")/venv/bin/activate"  
+source "$(dirname "$0")/venv/bin/activate"  # commenté pour éviter les erreurs de chemin si render est utilisé
 # puis lance Uvicorn
 exec uvicorn app:app \
      --host 127.0.0.1 \
